d8d336a53acf055f9de56e357831632febb3cb7c
Jemima <jemima.a.george@gmail.com>
2026-08-20 17:16:00 +0400

    Parent/child Components

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,4 @@
 // React renders all markup to the div with id=root using react jsx files
-import { createElement } from "react"
 import {createRoot} from "react-dom/client"
 
 // Can create own reusable component as a function using JS to render in html
@@ -7,25 +6,20 @@ function MainContent () {
     return <h3>React is cool!</h3>
 }
 
-// const root = createRoot(document.getElementById("root"))
-// // Render html or js content to the root
-// root.render(
-//     <div>
-//         <h1>Hello, React!</h1>
-//         < MainContent />
-//     </div>
-// )
-
-const h1 = document.createElement("h1")  
-h1.textContent = "This is Imperative code which is appended to the 'root' element."
-h1.className = "header"
-document.getElementById("root").appendChild(h1)
+const root = createRoot(document.getElementById("root"))
+// Render html or js content to the root
+root.render(
+    <main>
+        <h1 className="header">Hello, React!</h1>
+        < MainContent /> 
+        <img src="/src/react-logo.png" alt="React Logo"></img>
+    </main>
+)
 
 createRoot(document.querySelector("#list")).render(
     <ul>
         <li>Create root in React </li>
         <li>Render content into root div</li>
     </ul>
-
 )
 
