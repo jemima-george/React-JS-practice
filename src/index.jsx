d870898c36c02994716b926e89dfc4645432428a
Jemima <jemima.a.george@gmail.com>
2026-08-20 14:00:41 +0400

    First React Challenge

--- src/index.jsx
+++ src/index.jsx
@@ -4,3 +4,11 @@ import {createRoot} from "react-dom/client"
 const root = createRoot(document.getElementById("root"))
 // Render html or js content to the root
 root.render(<h1>Hello, React!</h1>)
+
+createRoot(document.querySelector("#list")).render(
+    <ul>
+        <li>Create root in React </li>
+        <li>Render content into root div</li>
+    </ul>
+
+)
