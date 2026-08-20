8bedca02872302ded965f8c19f9cfa3a2e771a14
Jemima <jemima.a.george@gmail.com>
2026-08-20 15:00:17 +0400

    Imperative Code - Append Child

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
 // React renders all markup to the div with id=root using react jsx files
+import { createElement } from "react"
 import {createRoot} from "react-dom/client"
 
 // Can create own reusable component as a function using JS to render in html
@@ -6,14 +7,19 @@ function MainContent () {
     return <h3>React is cool!</h3>
 }
 
-const root = createRoot(document.getElementById("root"))
-// Render html or js content to the root
-root.render(
-    <div>
-        <h1>Hello, React!</h1>
-        < MainContent />
-    </div>
-)
+// const root = createRoot(document.getElementById("root"))
+// // Render html or js content to the root
+// root.render(
+//     <div>
+//         <h1>Hello, React!</h1>
+//         < MainContent />
+//     </div>
+// )
+
+const h1 = document.createElement("h1")  
+h1.textContent = "This is Imperative code which is appended to the 'root' element."
+h1.className = "header"
+document.getElementById("root").appendChild(h1)
 
 createRoot(document.querySelector("#list")).render(
     <ul>
