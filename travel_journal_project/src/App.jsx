f5c11651957ef9b092b6fc81e0d4ef0c293bda48
Jemima <jemima.a.george@gmail.com>
2026-08-21 18:39:45 +0400

    Use Key Prop

--- travel_journal_project/src/App.jsx
+++ travel_journal_project/src/App.jsx
@@ -4,9 +4,11 @@ import data from "./data"
 
 export default function App () {
     // Map each entry in data and send data as props to Entry func 
+    // Each new element mapped in React needs a key to identify them seperately
     const entryElements = data.map((entry) => {
         return (
             <Entry 
+                key = {entry.id}
                 img = {entry.img}
                 title = {entry.title}
                 country = {entry.country}
