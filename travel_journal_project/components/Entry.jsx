b441ed8613c794332390c017ee10a0f64cdc6019
Jemima <jemima.a.george@gmail.com>
2026-08-21 18:51:20 +0400

    Pass Object through Props

--- travel_journal_project/components/Entry.jsx
+++ travel_journal_project/components/Entry.jsx
@@ -2,17 +2,17 @@ export default function Entry (props) {
     return (
         <article className="journal-entry">
             <div className="travel-img-container">
-                <img className="travel-img" src={props.img.src} alt={props.img.alt}/>
+                <img className="travel-img" src={props.entry.img.src} alt={props.entry.img.alt}/>
             </div>
             <div className="info-container">
                 <div className="info-location">
                     <i class="fas fa-map-marker-alt"></i>
-                    <h3 className="country">{props.country}</h3>
-                    <a href={props.googleMapsLink}>View on Google Maps</a>
+                    <h3 className="country">{props.entry.country}</h3>
+                    <a href={props.entry.googleMapsLink}>View on Google Maps</a>
                 </div>
-                <h2 className="entry-title">{props.title}</h2>
-                <p className="trip-dates">{props.dates}</p>
-                <p className="entry-text">{props.text}</p>
+                <h2 className="entry-title">{props.entry.title}</h2>
+                <p className="trip-dates">{props.entry.dates}</p>
+                <p className="entry-text">{props.entry.text}</p>
             </div>
         </article>
     )
