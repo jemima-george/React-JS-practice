ba73e978abc4beb35d16c949ecade7fa4fc90a41
Jemima <jemima.a.george@gmail.com>
2026-08-21 13:21:05 +0400

    Travel Journal - Header Challenge

---
+++ travel_journal_project/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header () {
+    return (
+        <header className="header">
+            <icon class="fas fa-globe-americas" alt="globe-con" ></icon>
+            <p className="header-text">my travel journal.</p>
+        </header>
+    )
+}
