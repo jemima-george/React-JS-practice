bcbbcefba94bca4f1e545fa69e1badd9cfab96ff
Jemima <jemima.a.george@gmail.com>
2026-08-24 15:05:11 +0400

    AI Chef - Form

---
+++ AI_chef_project/src/Main.jsx
@@ -0,0 +1,10 @@
+export default function Main (){
+    return (
+        <main>
+            <form className="add-ingredient-form">
+                <input aria-label="Add Ingredient" placeholder=" E.g: yogurt" name="ingredient"></input>
+                <button>+ Add Ingredient</button>
+            </form>
+        </main>
+    )
+}
