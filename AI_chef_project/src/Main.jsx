7e7902f72cc309750dbdb9d55aff7fadb0b30564
Jemima <jemima.a.george@gmail.com>
2026-08-24 17:48:07 +0400

    Handle user events and useState

--- AI_chef_project/src/Main.jsx
+++ AI_chef_project/src/Main.jsx
@@ -1,10 +1,31 @@
+// Use useState when original state changed to re-run page with new state 
+import React from "react"
+
 export default function Main (){
+    // Have to initialise state and react will re-run when set state changes
+    const [ingredients, setIngredients] = React.useState([])
+
+    const ingredientList = ingredients.map((ingredient) => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function handleSubmit(event){
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        // Set new state as a list with previous ingredients and new ingredient
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
+
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
                 <input aria-label="Add Ingredient" placeholder=" E.g: yogurt" name="ingredient"></input>
                 <button>+ Add Ingredient</button>
             </form>
+            <ul>
+                {ingredientList}
+            </ul>
         </main>
     )
 }
