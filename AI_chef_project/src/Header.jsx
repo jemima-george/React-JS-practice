414c833ef825568e1773b7f1106c738a2cf15a7d
Jemima <jemima.a.george@gmail.com>
2026-08-24 17:47:44 +0400

    Update Header Style

--- AI_chef_project/src/Header.jsx
+++ AI_chef_project/src/Header.jsx
@@ -1,9 +1,9 @@
 export default function Header () {
     return (
         <header className="header">
-            <icon class="fas fa-hamburger"></icon>
+            <i class="fas fa-hamburger"></i>
             <h1>Personal AI Chef</h1>
-            <icon class="fas fa-utensils"></icon>
+            <i class="fas fa-utensils"></i>
         </header> 
     )
 }
