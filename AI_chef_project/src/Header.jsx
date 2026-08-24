82accda9aedc7f98241944fdf1b7512fd1612f35
Jemima <jemima.a.george@gmail.com>
2026-08-24 13:40:45 +0400

    AI Chef - Header

---
+++ AI_chef_project/src/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header () {
+    return (
+        <header className="header">
+            <icon class="fas fa-hamburger"></icon>
+            <h1>Personal AI Chef</h1>
+            <icon class="fas fa-utensils"></icon>
+        </header> 
+    )
+}
