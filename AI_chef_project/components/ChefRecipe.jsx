1cbcd429590ac7d63d4023ae9066e238fbf9609b
Jemima <jemima.a.george@gmail.com>
2026-08-25 21:14:31 +0400

    Style Recipe Response

--- AI_chef_project/components/ChefRecipe.jsx
+++ AI_chef_project/components/ChefRecipe.jsx
@@ -1,7 +1,12 @@
+import Markdown from 'react-markdown'
+
 export default function ChefRecipe (props) {
     return (
-        <section>
-            {props.recipe}
+        <section className='suggested-recipe-container'>
+            <h2 className='suggested-recipe-header'>AI Chef Reccomends:</h2>
+            <Markdown>
+                {props.recipe}
+            </Markdown>
         </section>
     )
 }
